819cbcc8ccf382eb832504ad77598ea17b15bc71
klu2300031127 <163739584+klu2300031127@users.noreply.github.com>
2026-02-28 23:08:19 +0530

    Update discriminator.py

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,22 +1,27 @@
 import torch
 import torch.nn as nn
 
+
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Conv2d(3, 64, 4, 2, 1),
-            nn.LeakyReLU(0.2),
+            nn.Conv2d(4, 64, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
 
             nn.Conv2d(64, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Flatten(),
-            nn.Linear(128 * 16 * 16, 1),
+            nn.Conv2d(128, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(256, 1, 4, 1, 1),
             nn.Sigmoid()
         )
 
-    def forward(self, x):
+    def forward(self, gray, color):
+        x = torch.cat([gray, color], dim=1)
         return self.model(x)
